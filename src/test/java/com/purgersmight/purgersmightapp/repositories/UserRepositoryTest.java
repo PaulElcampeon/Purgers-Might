63d86092f0d43b0cc98a17ef956239fd084e4299
PaulElcampeon <pauloladele@live.co.uk>
2019-02-02 23:00:26 +0000

    tests for userRepository

--- src/test/java/com/purgersmight/purgersmightapp/repositories/UserRepositoryTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/repositories/UserRepositoryTest.java
@@ -1,4 +1,81 @@
 package com.purgersmight.purgersmightapp.repositories;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.purgersmight.purgersmightapp.User;
+import com.purgersmight.purgersmightapp.UserRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @After
+    public void clearRepository(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void addUser_Test1(){
+        User user = new User();
+        user.setUsername("Angie");
+        user.setPassword("43");
+        userRepository.insert(user);
+
+        long numberOfUsers = userRepository.count();
+
+        assertEquals(1, numberOfUsers);
+    }
+
+    @Test
+    public void addUser_Test2(){
+        User user = new User();
+        user.setUsername("Angie");
+        user.setPassword("43");
+
+        userRepository.insert(user);
+
+        assertThrows(DuplicateKeyException.class,()->{
+            userRepository.insert(user);
+        });
+    }
+
+    @Test
+    public void updateUser_Test3(){
+        User user = new User();
+        user.setUsername("Angie");
+        user.setPassword("43");
+
+        userRepository.insert(user);
+
+        user.setPassword("0000");
+
+        userRepository.save(user);
+
+        User resultUser = userRepository.findUserByUsername("Angie");
+        assertEquals("0000", resultUser.getPassword());
+    }
+
+    @Test
+    public void findUserByUsername_Test4(){
+        User user = new User();
+        user.setUsername("Angie");
+        user.setPassword("43");
+
+        userRepository.insert(user);
+
+        User resultUser = userRepository.findUserByUsername("Angie");
+        assertEquals(user, resultUser);
+    }
+
+
 }
