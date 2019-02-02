ac45f091fe335c636aab9311033ecbf4a628ef26
PaulElcampeon <pauloladele@live.co.uk>
2019-02-02 18:03:23 +0000

    first commit

---
+++ src/main/java/com/purgersmight/purgersmightapp/PurgersMightAppApplication.java
@@ -0,0 +1,14 @@
+package com.purgersmight.purgersmightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurgersMightAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurgersMightAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/purgersmight/purgersmightapp/PurgersMightAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.purgersmight.purgersmightapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PurgersMightAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
