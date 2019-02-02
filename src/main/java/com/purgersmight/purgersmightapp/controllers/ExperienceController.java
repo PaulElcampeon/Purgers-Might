a1c222599923598af9412f358d4aab4fa632441a
PaulElcampeon <pauloladele@live.co.uk>
2019-02-02 21:48:05 +0000

    added experienceService IT and controller

---
+++ src/main/java/com/purgersmight/purgersmightapp/controllers/ExperienceController.java
@@ -0,0 +1,24 @@
+package com.purgersmight.purgersmightapp.controllers;
+
+import com.purgersmight.purgersmightapp.services.ExperienceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExperienceController {
+
+    @Autowired
+    private ExperienceService experienceService;
+
+    @RequestMapping(value = "/experience-service/get-experience/{levelOfWinner}/{levelOfLoser}", method = RequestMethod.GET)
+    public ResponseEntity<String> getExperiencePoint(@PathVariable final int levelOfWinner, @PathVariable final int levelOfLoser){
+        return new ResponseEntity<>(
+                String.valueOf(experienceService.getExperience(levelOfWinner, levelOfLoser)),
+                HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/purgersmight/purgersmightapp/services/ExperienceServiceIT.java
@@ -0,0 +1,60 @@
+package com.purgersmight.purgersmightapp.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ExperienceServiceIT {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl = "http://localhost:";
+    @Before
+    public void initBaseUrl(){
+        baseUrl+= port;
+    }
+
+    @Test
+    public void getPlayerExperience_Test1(){
+        ResponseEntity<String> respEntity = testRestTemplate.getForEntity(baseUrl+"/experience-service/get-experience/5/5", String.class);
+        assertEquals(respEntity.getStatusCode(), HttpStatus.OK);
+        assertEquals(respEntity.getBody(), "20");
+    }
+
+    @Test
+    public void getPlayerExperience_Test2(){
+        ResponseEntity<String> respEntity = testRestTemplate.getForEntity(baseUrl+"/experience-service/get-experience/5/6", String.class);
+        assertEquals(respEntity.getStatusCode(), HttpStatus.OK);
+        assertEquals(respEntity.getBody(), "24");
+    }
+
+    @Test
+    public void getPlayerExperience_Test3(){
+        ResponseEntity<String> respEntity = testRestTemplate.getForEntity(baseUrl+"/experience-service/get-experience/6/5", String.class);
+        assertEquals(respEntity.getStatusCode(), HttpStatus.OK);
+        assertEquals(respEntity.getBody(), "16");
+    }
+
+    @Test
+    public void getPlayerExperience_Test4(){
+        ResponseEntity<String> respEntity = testRestTemplate.getForEntity(baseUrl+"/experience-service/get-experience/10/5", String.class);
+        assertEquals(respEntity.getStatusCode(), HttpStatus.OK);
+        assertEquals(respEntity.getBody(), "0");
+    }
+}
