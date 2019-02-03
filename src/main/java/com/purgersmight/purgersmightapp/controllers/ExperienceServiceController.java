d32c2b23113fbf8cf91909d2880a0d6af7d9d90a
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 14:15:20 +0000

    changed name of class

--- src/main/java/com/purgersmight/purgersmightapp/controllers/ExperienceServiceController.java
+++ src/main/java/com/purgersmight/purgersmightapp/controllers/ExperienceServiceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ExperienceController {
+public class ExperienceServiceController {
 
     @Autowired
     private ExperienceService experienceService;
