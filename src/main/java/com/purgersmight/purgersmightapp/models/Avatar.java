90cbb77e493a89ffb13af460ebe24160b5f1f5f3
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 22:30:31 +0000

    added an avatar abstract class

--- src/main/java/com/purgersmight/purgersmightapp/models/Avatar.java
+++ src/main/java/com/purgersmight/purgersmightapp/models/Avatar.java
@@ -1,4 +1,15 @@
 package com.purgersmight.purgersmightapp.models;
 
-public class Avatar {
+import lombok.Data;
+
+@Data
+public abstract class Avatar {
+
+    private String userName;
+    private String imageUrl;
+    private int level = 1;
+    private AvatarAttribute<Integer> health = new AvatarAttribute<>(100);
+    private AvatarAttribute<Integer> manna = new AvatarAttribute<>(60);
+    private SpellBook spellBook;
+
 }
