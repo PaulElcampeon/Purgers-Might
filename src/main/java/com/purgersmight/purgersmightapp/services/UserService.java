3ac4ea95360337bb4d4679dbd9f88db07f63b24b
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 13:21:21 +0000

    added functionality to encode user password

--- src/main/java/com/purgersmight/purgersmightapp/services/UserService.java
+++ src/main/java/com/purgersmight/purgersmightapp/services/UserService.java
@@ -1,4 +1,27 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.models.User;
+import com.purgersmight.purgersmightapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void addUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        userRepository.insert(newUser);
+    }
+
+    public User getUserByUsername(final String username){
+        return userRepository.findByUsername(username);
+    }
 }
