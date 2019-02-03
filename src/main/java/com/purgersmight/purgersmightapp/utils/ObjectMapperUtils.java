6adad4a61fbd3e8be8d0abf385a636acf2478031
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 22:26:33 +0000

    added objectmapper Utility class

--- src/main/java/com/purgersmight/purgersmightapp/utils/ObjectMapperUtils.java
+++ src/main/java/com/purgersmight/purgersmightapp/utils/ObjectMapperUtils.java
@@ -1,4 +1,12 @@
 package com.purgersmight.purgersmightapp.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ObjectMapperUtils {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static ObjectMapper getObjectMapper(){
+        return objectMapper;
+    }
 }
