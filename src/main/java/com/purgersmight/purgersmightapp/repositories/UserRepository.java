a2555f95c032749212db3dbd84bc0b6512ffc05f
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 13:14:18 +0000

    changed arg to final

--- src/main/java/com/purgersmight/purgersmightapp/repositories/UserRepository.java
+++ src/main/java/com/purgersmight/purgersmightapp/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
 
-    public User findByUsername(String username);
+    public User findByUsername(final String username);
 }
