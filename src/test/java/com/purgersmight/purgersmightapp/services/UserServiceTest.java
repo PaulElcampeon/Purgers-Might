aa75297dc908ab78d5b2836223187219691ecacb
PaulElcampeon <pauloladele@live.co.uk>
2019-02-03 13:21:36 +0000

    first test now passes

--- src/test/java/com/purgersmight/purgersmightapp/services/UserServiceTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/services/UserServiceTest.java
@@ -8,12 +8,14 @@ import com.purgersmight.purgersmightapp.models.User;
 import com.purgersmight.purgersmightapp.repositories.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class UserServiceTest {
 
     @Autowired
@@ -32,9 +34,12 @@ public class UserServiceTest {
 
         userService.addUser(user);
 
+        verify(userRepository, times(1)).insert(Mockito.any(User.class));
+
         User retrievedUser = userService.getUserByUsername("Angie");
 
         assertEquals(user, retrievedUser);
     }
+
 
 }
