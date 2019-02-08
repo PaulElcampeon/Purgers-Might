d2816d525cacebcb6399ff0374ac66bf8d7235e9
PaulElcampeon <pauloladele@live.co.uk>
2019-02-08 15:45:49 +0000

    removed empty spaces

--- src/main/java/com/purgersmight/purgersmightapp/services/UserService.java
+++ src/main/java/com/purgersmight/purgersmightapp/services/UserService.java
@@ -23,36 +23,28 @@ public class UserService {
     private Logger logger = Logger.getLogger(UserService.class.getName());
 
     public void addUser(User newUser){
-
         newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
-
         userRepository.insert(newUser);
-
         logger.log(Level.INFO, String.format("%s has just created an account", newUser.getUsername()));
     }
 
     public User getUserByUsername(final String username){
-
         return userRepository.findByUsername(username);
     }
 
     public void removeUser(final User user){
-
         userRepository.delete(user);
     }
 
     public void removeUserById(final String username){
-
         userRepository.deleteById(username);
     }
 
     public void updateUser(final User user){
-
         userRepository.save(user);
     }
 
     public void removeAllUsers(){
-
         userRepository.deleteAll();
     }
 }
