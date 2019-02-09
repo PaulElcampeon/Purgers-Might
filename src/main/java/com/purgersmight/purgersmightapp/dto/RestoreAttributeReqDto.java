b0b0433f521c6b2014c33dd18f7a4e3b6c1aa2af
PaulElcampeon <pauloladele@live.co.uk>
2019-02-09 11:50:26 +0000

    restoreAttributeReqDto

--- src/main/java/com/purgersmight/purgersmightapp/dto/RestoreAttributeReqDto.java
+++ src/main/java/com/purgersmight/purgersmightapp/dto/RestoreAttributeReqDto.java
@@ -1,4 +1,14 @@
 package com.purgersmight.purgersmightapp.dto;
 
+import lombok.Data;
+
+@Data
 public class RestoreAttributeReqDto {
+    private String username;
+
+    public RestoreAttributeReqDto(){}
+
+    public RestoreAttributeReqDto(String username){
+        this.username = username;
+    }
 }
