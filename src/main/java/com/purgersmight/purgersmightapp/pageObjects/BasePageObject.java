0bfd1504d493c4e23d055c3776831fa5adeb5a3e
PaulElcampeon <pauloladele@live.co.uk>
2019-02-10 01:21:34 +0000

    added parent class for pages for selenium tests

--- src/main/java/com/purgersmight/purgersmightapp/pageObjects/BasePageObject.java
+++ src/main/java/com/purgersmight/purgersmightapp/pageObjects/BasePageObject.java
@@ -1,4 +1,17 @@
 package com.purgersmight.purgersmightapp.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePageObject {
+
+    protected WebDriver driver;
+
+    public BasePageObject(WebDriver driver){
+        this.driver = driver;
+    }
+
+    protected void openUrl(String url){
+        System.out.printf("%nOpening page %s",url);
+        driver.get(url);
+    }
 }
--- src/test/java/com/purgersmight/purgersmightapp/webBrower/BaseTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/webBrower/BaseTest.java
@@ -1,14 +1,23 @@
-package com.purgersmight.purgersmightapp.pageObjects;
+package com.purgersmight.purgersmightapp.webBrower;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @Before
     public void setUp(){
+        System.out.println("Setting up driver");
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
-
+    @After
+    public void tearDown(){
+        System.out.println("Closing driver");
+        driver.quit();
+    }
 }
