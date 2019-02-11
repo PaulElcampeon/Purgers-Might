04d1ce46750394e2ab81ebe03c2a968b976ca21a
PaulElcampeon <pauloladele@live.co.uk>
2019-02-11 16:46:45 +0000

    added new functionality for avater leveling

--- src/main/java/com/purgersmight/purgersmightapp/services/LevelService.java
+++ src/main/java/com/purgersmight/purgersmightapp/services/LevelService.java
@@ -1,4 +1,53 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.models.Avatar;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+@Scope(value = "singleton")
 public class LevelService {
+
+    @Value("${levelup.bonus.experience}")
+    private double experienceBonus;
+
+    @Value("${levelup.bonus.health}")
+    private double healthBonus;
+
+    @Value("${levelup.bonus.manna}")
+    private double mannaBonus;
+
+    private Logger logger = Logger.getLogger(LevelService.class.getName());
+
+    public void levelUpAvatar(Avatar avatar) {
+
+        if (avatar.getExperience().getRunning() >= avatar.getExperience().getActual()) {
+
+            avatar.setLevel(avatar.getLevel() + 1);
+
+            avatar.getExperience().setRunning(avatar.getExperience().getRunning() - avatar.getExperience().getActual());
+
+            levelAvatarAttributes(avatar);
+
+            logger.log(Level.INFO, String.format("%s is now level %d", avatar.getUsername(), avatar.getLevel()));
+
+            if (avatar.getExperience().getRunning() >= avatar.getExperience().getActual()) {
+
+                levelUpAvatar(avatar);
+            }
+        }
+    }
+
+    public void levelAvatarAttributes(Avatar avatar) {
+
+        avatar.getExperience().setActual((int) (avatar.getExperience().getActual() * experienceBonus));
+
+        avatar.getHealth().setActual((int) (avatar.getHealth().getActual() * healthBonus));
+
+        avatar.getManna().setActual((int) (avatar.getManna().getActual() * mannaBonus));
+    }
 }
--- src/test/java/com/purgersmight/purgersmightapp/services/LevelServiceTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/services/LevelServiceTest.java
@@ -1,4 +1,126 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.PurgersMightAppApplication;
+import com.purgersmight.purgersmightapp.config.WebSecurityConfig;
+import com.purgersmight.purgersmightapp.models.Avatar;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PurgersMightAppApplication.class, WebSecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class LevelServiceTest {
+
+    @Autowired
+    private LevelService levelService;
+
+    @Test
+    public void levelUpAvatar_shouldBeLevel2ExperienceShouldBe0_Test1() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        avatar.setLevel(1);
+
+        avatar.getExperience().setRunning(100);
+
+        levelService.levelUpAvatar(avatar);
+
+        assertEquals(2, avatar.getLevel());
+
+        assertEquals(0, avatar.getExperience().getRunning().intValue());
+
+        assertEquals(110, avatar.getExperience().getActual().intValue());
+    }
+
+    @Test
+    public void levelUpAvatar_shouldBeLevel2ExperienceShouldBe100_Test2() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        avatar.setLevel(1);
+
+        avatar.getExperience().setRunning(200);
+
+        levelService.levelUpAvatar(avatar);
+
+        assertEquals(2, avatar.getLevel());
+
+        assertEquals(100, avatar.getExperience().getRunning().intValue());
+
+        assertEquals(110, avatar.getExperience().getActual().intValue());
+    }
+
+    @Test
+    public void levelUpAvatar_shouldBeLevel3ExperienceShouldBe19_Test3() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        avatar.setLevel(1);
+
+        avatar.getExperience().setRunning(350);
+
+        levelService.levelUpAvatar(avatar);
+
+        assertEquals(4, avatar.getLevel());
+
+        assertEquals(19, avatar.getExperience().getRunning().intValue());
+
+        assertEquals(133, avatar.getExperience().getActual().intValue());
+    }
+
+    @Test
+    public void levelUpAvatar_shouldBeLevel1ExperienceShouldBe90_Test4() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        avatar.setLevel(1);
+
+        avatar.getExperience().setRunning(90);
+
+        levelService.levelUpAvatar(avatar);
+
+        assertEquals(1, avatar.getLevel());
+
+        assertEquals(90, avatar.getExperience().getRunning().intValue());
+
+        assertEquals(100, avatar.getExperience().getActual().intValue());
+    }
+
+    @Test
+    public void levelAvatarAttributes_health105Manna63Exp110_Test5() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        levelService.levelAvatarAttributes(avatar);
+
+        assertEquals(105, avatar.getHealth().getActual().intValue());
+
+        assertEquals(63, avatar.getManna().getActual().intValue());
+
+        assertEquals(110, avatar.getExperience().getActual().intValue());
+    }
+
+    @Test
+    public void levelAvatarAttributes_health110Manna66Exp121_Test6() {
+
+        Avatar avatar = Avatar.getStarterAvatar("Dave");
+
+        avatar.getExperience().setActual(110);
+
+        avatar.getHealth().setActual(105);
+
+        avatar.getManna().setActual(63);
+
+        levelService.levelAvatarAttributes(avatar);
+
+        assertEquals(110, avatar.getHealth().getActual().intValue());
+
+        assertEquals(66, avatar.getManna().getActual().intValue());
+
+        assertEquals(121, avatar.getExperience().getActual().intValue());
+    }
 }
