a6bce766dc3445d26ecc22e8bb7e4939c1b98f33
PaulElcampeon <pauloladele@live.co.uk>
2019-02-11 17:12:50 +0000

    added functionality for awarding winner of pvp event

--- src/main/java/com/purgersmight/purgersmightapp/services/AwardService.java
+++ src/main/java/com/purgersmight/purgersmightapp/services/AwardService.java
@@ -1,4 +1,50 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.models.Avatar;
+import com.purgersmight.purgersmightapp.models.PvpEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope(value = "singleton")
 public class AwardService {
+
+    @Autowired
+    private ExperienceService experienceService;
+
+    @Autowired
+    private LevelService levelService;
+
+    @Value("${kenja.points.pvp}")
+    private int kenjaPointsPvpAward;
+
+    public void awardWinningPlayer(PvpEvent pvpEvent) {
+
+        if (pvpEvent.getPlayer1().getHealth().getRunning() == 0) {
+
+            awardExperienceAndLevelAndKenjaPoints(pvpEvent.getPlayer2(), pvpEvent.getPlayer1());
+
+        } else {
+
+            awardExperienceAndLevelAndKenjaPoints(pvpEvent.getPlayer1(), pvpEvent.getPlayer2());
+        }
+    }
+
+    public void awardExperienceAndLevelAndKenjaPoints(Avatar winner, Avatar loser) {
+
+        int awardedExperience = experienceService.getExperience(winner.getLevel(), loser.getLevel());
+
+        winner.getExperience().setRunning(winner.getExperience().getRunning() + awardedExperience);
+
+        levelService.levelUpAvatar(winner);
+
+        awardKenjaPoints(winner);
+    }
+
+    public void awardKenjaPoints(Avatar avatar) {
+
+        avatar.setKenjaPoints(avatar.getKenjaPoints() + kenjaPointsPvpAward);
+    }
 }
--- src/test/java/com/purgersmight/purgersmightapp/services/AwardServiceTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/services/AwardServiceTest.java
@@ -1,4 +1,154 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.PurgersMightAppApplication;
+import com.purgersmight.purgersmightapp.config.WebSecurityConfig;
+import com.purgersmight.purgersmightapp.models.Avatar;
+import com.purgersmight.purgersmightapp.models.PvpEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PurgersMightAppApplication.class, WebSecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class AwardServiceTest {
+
+    @Autowired
+    private AwardService awardService;
+
+    @Test
+    public void awardKenjaPoints_shouldRecieve2KenjaPoint_Test1(){
+
+        Avatar avatar = Avatar.getStarterAvatar("Danny");
+
+        avatar.setKenjaPoints(0);
+
+        awardService.awardKenjaPoints(avatar);
+
+        assertEquals(2, avatar.getKenjaPoints());
+    }
+
+    @Test
+    public void awardExperienceAndLevelAndKenjaPoints__Test2(){
+
+        Avatar avatar1 = Avatar.getStarterAvatar("Danny");
+
+        Avatar avatar2 = Avatar.getStarterAvatar("Richie");
+
+        awardService.awardExperienceAndLevelAndKenjaPoints(avatar1, avatar2);
+
+        //Winner
+        assertEquals(20, avatar1.getExperience().getRunning().intValue());
+
+        assertEquals(2, avatar1.getKenjaPoints());
+
+        assertEquals(1, avatar1.getLevel());
+
+        //Loser
+        assertEquals(0, avatar2.getExperience().getRunning().intValue());
+
+        assertEquals(0, avatar2.getKenjaPoints());
+
+        assertEquals(1, avatar2.getLevel());
+    }
+
+    @Test
+    public void awardExperienceAndLevelAndKenjaPoints__Test3(){
+
+        Avatar avatar1 = Avatar.getStarterAvatar("Danny");
+
+        avatar1.getExperience().setRunning(90);
+
+        Avatar avatar2 = Avatar.getStarterAvatar("Richie");
+
+        awardService.awardExperienceAndLevelAndKenjaPoints(avatar1, avatar2);
+
+        //Winner
+        assertEquals(10, avatar1.getExperience().getRunning().intValue());
+
+        assertEquals(110, avatar1.getExperience().getActual().intValue());
+
+        assertEquals(2, avatar1.getKenjaPoints());
+
+        assertEquals(2, avatar1.getLevel());
+
+        //Loser
+        assertEquals(0, avatar2.getExperience().getRunning().intValue());
+
+        assertEquals(0, avatar2.getKenjaPoints());
+
+        assertEquals(1, avatar2.getLevel());
+    }
+
+    @Test
+    public void awardWinningPlayer__Test4(){
+
+        Avatar avatar1 = Avatar.getStarterAvatar("Danny");
+
+        Avatar avatar2 = Avatar.getStarterAvatar("Richie");
+
+        avatar2.getHealth().setRunning(0);
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(avatar1);
+
+        pvpEvent.setPlayer2(avatar2);
+
+        awardService.awardExperienceAndLevelAndKenjaPoints(avatar1, avatar2);
+
+        //Winner
+        assertEquals(20, avatar1.getExperience().getRunning().intValue());
+
+        assertEquals(2, avatar1.getKenjaPoints());
+
+        assertEquals(1, avatar1.getLevel());
+
+        //Loser
+        assertEquals(0, avatar2.getExperience().getRunning().intValue());
+
+        assertEquals(0, avatar2.getKenjaPoints());
+
+        assertEquals(1, avatar2.getLevel());
+    }
+
+    @Test
+    public void awardWinningPlayer__Test5(){
+
+        Avatar avatar1 = Avatar.getStarterAvatar("Danny");
+
+        avatar1.getExperience().setRunning(90);
+
+        Avatar avatar2 = Avatar.getStarterAvatar("Richie");
+
+        avatar2.getHealth().setRunning(0);
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(avatar1);
+
+        pvpEvent.setPlayer2(avatar2);
+
+        awardService.awardExperienceAndLevelAndKenjaPoints(avatar1, avatar2);
+
+        //Winner
+        assertEquals(10, avatar1.getExperience().getRunning().intValue());
+
+        assertEquals(110, avatar1.getExperience().getActual().intValue());
+
+        assertEquals(2, avatar1.getKenjaPoints());
+
+        assertEquals(2, avatar1.getLevel());
+
+        //Loser
+        assertEquals(0, avatar2.getExperience().getRunning().intValue());
+
+        assertEquals(0, avatar2.getKenjaPoints());
+
+        assertEquals(1, avatar2.getLevel());
+    }
 }
+
