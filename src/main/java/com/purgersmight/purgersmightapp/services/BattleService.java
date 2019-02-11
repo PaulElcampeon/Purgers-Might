54dd6de7270c64c3e2a7af5e3408047f699ed699
PaulElcampeon <pauloladele@live.co.uk>
2019-02-11 08:26:25 +0000

    added a new service to deal with logic for player battle

--- src/main/java/com/purgersmight/purgersmightapp/services/BattleService.java
+++ src/main/java/com/purgersmight/purgersmightapp/services/BattleService.java
@@ -1,4 +1,202 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.dto.AttackPlayerReqDto;
+import com.purgersmight.purgersmightapp.dto.AttackPlayerResDto;
+import com.purgersmight.purgersmightapp.enums.AttackType;
+import com.purgersmight.purgersmightapp.enums.SpellType;
+import com.purgersmight.purgersmightapp.models.Avatar;
+import com.purgersmight.purgersmightapp.models.PvpEvent;
+import com.purgersmight.purgersmightapp.models.Spell;
+import com.purgersmight.purgersmightapp.utils.BattleUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+@Scope(value = "singleton")
 public class BattleService {
+
+    @Autowired
+    private PvpEventService pvpEventService;
+
+    @Autowired
+    private AvatarService avatarService;
+
+    @Autowired
+    private AwardService awardService;
+
+    private Logger logger = Logger.getLogger(BattleService.class.getName());
+
+    public AttackPlayerResDto processPlayerAttackDto(final AttackPlayerReqDto attackPlayerReqDto) {
+
+        PvpEvent pvpEvent = pvpEventService.getPvpEventByEventId(attackPlayerReqDto.getEventId());
+
+        Avatar actingAvatar;
+
+        Avatar defendingAvatar;
+
+        if (pvpEvent.getWhosTurn().equals(pvpEvent.getPlayer1().getUsername())) {
+
+            actingAvatar = pvpEvent.getPlayer1();
+
+            defendingAvatar = pvpEvent.getPlayer2();
+
+        } else {
+
+            actingAvatar = pvpEvent.getPlayer2();
+
+            defendingAvatar = pvpEvent.getPlayer1();
+        }
+
+        playerAttack(actingAvatar, defendingAvatar, attackPlayerReqDto);
+
+        if (checkIfEventEnded(pvpEvent)) {
+
+            logger.log(Level.INFO, String.format("PvP Event with id %s has now ended", pvpEvent.getEventId()));
+
+            awardService.awardWinningPlayer(pvpEvent);
+
+            pvpEventService.removePvpEventById(pvpEvent.getEventId());
+
+            updateAvatarsInDB(pvpEvent.getPlayer1(), pvpEvent.getPlayer2());
+
+            return new AttackPlayerResDto(true, pvpEvent.getWhosTurn(), pvpEvent);
+        }
+
+        changeWhosTurn(pvpEvent);
+
+        updatePvpEventInDB(pvpEvent);
+
+        updateAvatarsInDB(pvpEvent.getPlayer1(), pvpEvent.getPlayer2());
+
+        return new AttackPlayerResDto(false, null, pvpEvent);
+    }
+
+    public void playerAttack(Avatar actingAvatar, Avatar defendingAvatar, final AttackPlayerReqDto attackPlayerReqDto) {
+
+        if (attackPlayerReqDto.getAttackType().equals(AttackType.MELEE)) {
+
+            playerAttackMelee(actingAvatar, defendingAvatar);
+
+
+        } else {
+
+            playerAttackSpell(actingAvatar, defendingAvatar, attackPlayerReqDto);
+        }
+    }
+
+    public void playerAttackMelee(Avatar actingAvatar, Avatar defendingAvatar) {
+
+        int weaponDamage = BattleUtils.WeaponManager.getDamagePoints(actingAvatar.getWeapon());
+
+        double fullArmourPoint = BattleUtils.ArmourManager.getFullAmourDefensePoints(defendingAvatar.getBodyArmour());
+
+        double damageReduction = BattleUtils.DamageManager.getDamageReduction(fullArmourPoint);
+
+        int finalDamage = BattleUtils.DamageManager.getFinalDamage(weaponDamage, damageReduction);
+
+        setAvatarHealthAfterAttack(defendingAvatar, finalDamage);
+
+        autoCorrectPlayerHealth(defendingAvatar);
+    }
+
+    public void playerAttackSpell(Avatar actingAvatar, Avatar defendingAvatar, final AttackPlayerReqDto attackPlayerReqDto) {
+
+        Spell spell = actingAvatar.getSpellBook().getSpellList().get(attackPlayerReqDto.getSpellPosition());
+
+        if (spell.getSpellType().equals(SpellType.HEAL)) {
+
+            playerHeal(actingAvatar, spell);
+
+        } else {
+
+            if (actingAvatar.getManna().getRunning() >= spell.getMannaCost()) {
+
+                setAvatarHealthAfterAttack(defendingAvatar, spell.getDamagePoints());
+
+                setAvatarManna(actingAvatar, spell.getMannaCost());
+
+                autoCorrectPlayerHealth(defendingAvatar);
+            }
+        }
+    }
+
+    public void playerHeal(Avatar actingAvatar, final Spell healSpell) {
+
+        if (actingAvatar.getManna().getRunning() >= healSpell.getMannaCost()) {
+
+            setAvatarHealthAfterHeal(actingAvatar, healSpell.getDamagePoints());
+
+            setAvatarManna(actingAvatar, healSpell.getMannaCost());
+        }
+
+        autoCorrectPlayerHealth(actingAvatar);
+    }
+
+    public void autoCorrectPlayerHealth(Avatar avatar) {
+
+        if (avatar.getHealth().getRunning() > avatar.getHealth().getActual()) {
+
+            avatar.getHealth().setRunning(avatar.getHealth().getActual());
+        }
+
+        if (avatar.getHealth().getRunning() < 0) {
+
+            avatar.getHealth().setRunning(0);
+        }
+    }
+
+    public void setAvatarManna(Avatar avatar, int mannaCost) {
+
+        avatar.getManna().setRunning(avatar.getManna().getRunning() - mannaCost);
+
+        if (avatar.getManna().getRunning() < 0) {
+
+            avatar.getManna().setRunning(0);
+        }
+    }
+
+    public void setAvatarHealthAfterAttack(Avatar avatar, int damage) {
+
+        avatar.getHealth().setRunning(avatar.getHealth().getRunning() - damage);
+    }
+
+    public void setAvatarHealthAfterHeal(Avatar avatar, int healPoints) {
+
+        avatar.getHealth().setRunning(avatar.getHealth().getRunning() + healPoints);
+    }
+
+    public boolean checkIfEventEnded(PvpEvent pvpEvent) {
+
+        return pvpEvent.getPlayer1().getHealth().getRunning() == 0 || pvpEvent.getPlayer2().getHealth().getRunning() == 0;
+
+    }
+
+    public void changeWhosTurn(PvpEvent pvpEvent) {
+
+        if (pvpEvent.getWhosTurn().equals(pvpEvent.getPlayer1().getUsername())) {
+
+            pvpEvent.setWhosTurn(pvpEvent.getPlayer2().getUsername());
+
+        } else {
+
+            pvpEvent.setWhosTurn(pvpEvent.getPlayer1().getUsername());
+
+        }
+    }
+
+    private void updatePvpEventInDB(PvpEvent pvpEvent) {
+
+        pvpEventService.updatePvpEvent(pvpEvent);
+    }
+
+    private void updateAvatarsInDB(Avatar avatar1, Avatar avatar2) {
+
+        avatarService.updateAvatar(avatar1);
+
+        avatarService.updateAvatar(avatar2);
+    }
 }
--- src/test/java/com/purgersmight/purgersmightapp/services/BattleServiceTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/services/BattleServiceTest.java
@@ -1,4 +1,579 @@
 package com.purgersmight.purgersmightapp.services;
 
+import com.purgersmight.purgersmightapp.PurgersMightAppApplication;
+import com.purgersmight.purgersmightapp.config.WebSecurityConfig;
+import com.purgersmight.purgersmightapp.dto.AttackPlayerReqDto;
+import com.purgersmight.purgersmightapp.dto.AttackPlayerResDto;
+import com.purgersmight.purgersmightapp.enums.AttackType;
+import com.purgersmight.purgersmightapp.models.Avatar;
+import com.purgersmight.purgersmightapp.models.PvpEvent;
+import com.purgersmight.purgersmightapp.models.Spell;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PurgersMightAppApplication.class, WebSecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class BattleServiceTest {
+
+    @Autowired
+    private BattleService battleService;
+
+    @MockBean
+    private PvpEventService pvpEventService;
+
+    @MockBean
+    private AwardService awardService;
+
+    @MockBean
+    private AvatarService avatarService;
+
+    @Test
+    public void playerAttackMelee_playersHealthShouldReduce_Test1() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        battleService.playerAttackMelee(attacker, defender);
+
+        assertTrue(defender.getHealth().getRunning() < defender.getHealth().getActual());
+    }
+
+    @Test
+    public void playerAttackSpell_defendersHealthReduceAndAttacksMannaReduce_Test2() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttackSpell(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(80, defender.getHealth().getRunning().intValue());
+
+        assertEquals(50, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttackSpell_notEnoughMannaHealthAndMannaStaySame_Test3() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getManna().setRunning(5);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttackSpell(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(100, defender.getHealth().getRunning().intValue());
+
+        assertEquals(5, attacker.getManna().getRunning().intValue());
+    }
+
+
+    @Test
+    public void playerAttackSpellDirectToHeal_playersHealthShouldIncrease_Test4() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(40);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell healSpell = Spell.getDefaultHealSpell(15, 30);
+
+        attacker.getSpellBook().getSpellList().add(healSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttackSpell(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(70, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(45, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttackSpellDirectToHeal_notEnoughMannaHealthMannaStaySame_Test5() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(40);
+
+        attacker.getManna().setRunning(5);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell healSpell = Spell.getDefaultAttackSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(healSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttackSpell(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(40, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(5, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerHeal_playersHealthShouldIncrease_Test6() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(40);
+
+        Spell healSpell = Spell.getDefaultHealSpell(15, 30);
+
+        battleService.playerHeal(attacker, healSpell);
+
+        assertEquals(70, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(45, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerHeal_notEnoughMannaHealthMannaStaySame_Test7() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(40);
+
+        attacker.getManna().setRunning(5);
+
+        Spell healSpell = Spell.getDefaultAttackSpell(10, 20);
+
+        battleService.playerHeal(attacker, healSpell);
+
+        assertEquals(40, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(5, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void autoCorrectPlayerHealth_healthShouldBeSetToActual100_Test8() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(120);
+
+        battleService.autoCorrectPlayerHealth(attacker);
+
+        assertEquals(100, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void autoCorrectPlayerHealth_healthShouldBe0_Test9() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(-50);
+
+        battleService.autoCorrectPlayerHealth(attacker);
+
+        assertEquals(0, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void autoCorrectPlayerHealth_healthShouldNotChange_Test10() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(80);
+
+        battleService.autoCorrectPlayerHealth(attacker);
+
+        assertEquals(80, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void setAvatarManna_mannaShouldBeReducedTo70_Test11() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getManna().setRunning(80);
+
+        battleService.setAvatarManna(attacker, 10);
+
+        assertEquals(70, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void setAvatarManna_mannaShouldBeCorrectedTo0_Test12() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getManna().setRunning(30);
+
+        battleService.setAvatarManna(attacker, 40);
+
+        assertEquals(0, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void setAvatarHealthAfterAttack_healthShouldBeReduced_Test13() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(100);
+
+        battleService.setAvatarHealthAfterAttack(attacker, 40);
+
+        assertEquals(60, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void setAvatarHealthAfterHeal_healthShouldBeIncreasedTo80_Test14() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(65);
+
+        battleService.setAvatarHealthAfterHeal(attacker, 15);
+
+        assertEquals(80, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void checkIfEventEnded_shouldReturnTrue_Test15() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(0);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        boolean result = battleService.checkIfEventEnded(pvpEvent);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkIfEventEnded_shouldReturnFalse_Test16() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        boolean result = battleService.checkIfEventEnded(pvpEvent);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void changeWhosTurn_shouldBeDavesTurn_Test17() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        pvpEvent.setWhosTurn("Stanley");
+
+        battleService.changeWhosTurn(pvpEvent);
+
+        assertEquals("Dave", pvpEvent.getWhosTurn());
+    }
+
+    @Test
+    public void playerAttack_defendersHealthShouldBeReducedWithMelee_Test18() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        defender.getHealth().setRunning(100);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.MELEE, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertTrue(defender.getHealth().getRunning() < 100);
+    }
+
+    @Test
+    public void playerAttack_defendersHealthShouldBeReducedWithSpell_Test19() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getManna().setRunning(50);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        defender.getHealth().setRunning(100);
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(80, defender.getHealth().getRunning().intValue());
+
+        assertEquals(40, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttack_attackersHealthShouldBeIncreased_Test20() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(50);
+
+        attacker.getManna().setRunning(40);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell healSpell = Spell.getDefaultHealSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(healSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(70, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(30, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttack_attackersHealthShouldBeTheSameNotEnoughMannaToHeal_Test21() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(50);
+
+        attacker.getManna().setRunning(5);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell healSpell = Spell.getDefaultHealSpell(10, 20);
+
+        attacker.getSpellBook().getSpellList().add(healSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(50, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(5, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttack_defendersHealthShouldBe0WithSpellAttack_Test22() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getManna().setRunning(50);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        defender.getHealth().setRunning(30);
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 40);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.SPELL, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(0, defender.getHealth().getRunning().intValue());
+
+        assertEquals(40, attacker.getManna().getRunning().intValue());
+    }
+
+    @Test
+    public void playerAttack_defendersHealthShouldBe0WithMeleeAttack_Test23() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        defender.getHealth().setRunning(1);
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto(AttackType.MELEE, 0);
+
+        battleService.playerAttack(attacker, defender, attackPlayerReqDto);
+
+        assertEquals(0, defender.getHealth().getRunning().intValue());
+
+        assertEquals(60, attacker.getManna().getRunning().intValue());
+
+        assertEquals(100, attacker.getHealth().getRunning().intValue());
+    }
+
+    @Test
+    public void processPlayerAttackDto_attackingWithMelee_Test24() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        pvpEvent.setWhosTurn("Dave");
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto("MockEvent", AttackType.MELEE, 0);
+
+        when(pvpEventService.getPvpEventByEventId("MockEvent")).thenReturn(pvpEvent);
+
+        AttackPlayerResDto result = battleService.processPlayerAttackDto(attackPlayerReqDto);
+
+        assertTrue(defender.getHealth().getRunning() < 100);
+
+        assertFalse(result.isEnded());
+
+        assertEquals("Stanley", result.getPvpEvent().getWhosTurn());
+    }
+
+    @Test
+    public void processPlayerAttackDto_attackingWithSpell_Test25() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 40);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        pvpEvent.setWhosTurn("Dave");
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto("MockEvent", AttackType.SPELL, 0);
+
+        when(pvpEventService.getPvpEventByEventId("MockEvent")).thenReturn(pvpEvent);
+
+        AttackPlayerResDto result = battleService.processPlayerAttackDto(attackPlayerReqDto);
+
+        assertEquals(60, defender.getHealth().getRunning().intValue());
+
+        assertEquals(50, attacker.getManna().getRunning().intValue());
+
+        assertFalse(result.isEnded());
+
+        assertEquals("Stanley", result.getPvpEvent().getWhosTurn());
+    }
+
+    @Test
+    public void processPlayerAttackDto_healingWithSpell_Test25() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        attacker.getHealth().setRunning(40);
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        Spell healSpell = Spell.getDefaultHealSpell(10, 40);
+
+        attacker.getSpellBook().getSpellList().add(healSpell);
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        pvpEvent.setWhosTurn("Dave");
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto("MockEvent", AttackType.SPELL, 0);
+
+        when(pvpEventService.getPvpEventByEventId("MockEvent")).thenReturn(pvpEvent);
+
+        AttackPlayerResDto result = battleService.processPlayerAttackDto(attackPlayerReqDto);
+
+        assertEquals(80, attacker.getHealth().getRunning().intValue());
+
+        assertEquals(50, attacker.getManna().getRunning().intValue());
+
+        assertFalse(result.isEnded());
+
+        assertEquals("Stanley", result.getPvpEvent().getWhosTurn());
+    }
+
+    @Test
+    public void processPlayerAttackDto_eventShouldHaveEnded_Test26() {
+
+        Avatar attacker = Avatar.getStarterAvatar("Dave");
+
+        Avatar defender = Avatar.getStarterAvatar("Stanley");
+
+        defender.getHealth().setRunning(10);
+
+        Spell attackSpell = Spell.getDefaultAttackSpell(10, 40);
+
+        attacker.getSpellBook().getSpellList().add(attackSpell);
+
+        PvpEvent pvpEvent = new PvpEvent();
+
+        pvpEvent.setPlayer1(attacker);
+
+        pvpEvent.setPlayer2(defender);
+
+        pvpEvent.setWhosTurn("Dave");
+
+        AttackPlayerReqDto attackPlayerReqDto = new AttackPlayerReqDto("MockEvent", AttackType.SPELL, 0);
+
+        when(pvpEventService.getPvpEventByEventId("MockEvent")).thenReturn(pvpEvent);
+
+        AttackPlayerResDto result = battleService.processPlayerAttackDto(attackPlayerReqDto);
+
+        assertEquals(50, attacker.getManna().getRunning().intValue());
+
+        assertEquals(0, defender.getHealth().getRunning().intValue());
+
+        assertTrue(result.isEnded());
+    }
+
 }
