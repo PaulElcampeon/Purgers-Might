a255b2158d03b16b9d79945af57f541840ec9bc0
PaulElcampeon <pauloladele@live.co.uk>
2019-02-12 18:26:27 +0000

    completed first test

--- src/test/java/com/purgersmight/purgersmightapp/webBrower/LoginTest.java
+++ src/test/java/com/purgersmight/purgersmightapp/webBrower/LoginTest.java
@@ -4,14 +4,13 @@ import com.purgersmight.purgersmightapp.PurgersMightAppApplication;
 import com.purgersmight.purgersmightapp.config.WebSecurityConfig;
 import com.purgersmight.purgersmightapp.models.Avatar;
 import com.purgersmight.purgersmightapp.models.User;
+import com.purgersmight.purgersmightapp.pageObjects.HomePage;
 import com.purgersmight.purgersmightapp.pageObjects.LoginPage;
 import com.purgersmight.purgersmightapp.services.AvatarService;
 import com.purgersmight.purgersmightapp.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {PurgersMightAppApplication.class, WebSecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LoginTest extends BaseTest{
+public class LoginTest extends BaseTest {
 
     @LocalServerPort
     private int port;
@@ -36,7 +35,9 @@ public class LoginTest extends BaseTest{
 
     @Test
     public void login_success_Test1() {
-        LoginPage loginPage = new LoginPage(driver,port);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        LoginPage loginPage = new LoginPage(driver, port);
         loginPage.open();
 
         userService.removeAllUsers();
@@ -51,15 +52,43 @@ public class LoginTest extends BaseTest{
         LoginPage.password(driver).sendKeys("123456");
         LoginPage.submit(driver).click();
 
+        HomePage homePage = loginPage.getHomePage();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
+
+        String username = homePage.username(driver).getText();
+
+        String level = homePage.level(driver).getText();
+
+        String kenjaPoints = homePage.kenjaPoints(driver).getText();
+
+        String healthText = homePage.healthText(driver).getText();
+
+        String mannaText = homePage.mannaText(driver).getText();
+
+        String experienceText = homePage.experienceText(driver).getText();
+
         userService.removeAllUsers();
         avatarService.removeAllAvatars();
+
+        assertEquals("Username: Angie1", username);
+
+        assertEquals("Level: 1", level);
+
+        assertEquals("Kenja Points: 0", kenjaPoints);
+
+        assertEquals("100/100", healthText);
+
+        assertEquals("60/60", mannaText);
+
+        assertEquals("0/100", experienceText);
     }
 
     @Test
     public void login_error_Test2() {
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        LoginPage loginPage = new LoginPage(driver,port);
+        LoginPage loginPage = new LoginPage(driver, port);
         loginPage.open();
 
         LoginPage.username(driver).sendKeys("Angie1");
