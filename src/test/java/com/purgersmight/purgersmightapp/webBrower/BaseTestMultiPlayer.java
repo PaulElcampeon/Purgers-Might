5578f130e6b02c6f275f968b416932802bd46af9
PaulElcampeon <pauloladele@live.co.uk>
2019-02-13 15:55:08 +0000

    added a baseTestMultiplayer for multiplayer testing

--- src/test/java/com/purgersmight/purgersmightapp/webBrower/BaseTestMultiPlayer.java
+++ src/test/java/com/purgersmight/purgersmightapp/webBrower/BaseTestMultiPlayer.java
@@ -5,19 +5,24 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseTest {
-    protected WebDriver driver;
+public class BaseTestMultiPlayer {
+    protected WebDriver driver1;
+    protected WebDriver driver2;
+
 
     @Before
-    public void setUp(WebDriver... webDrivers) {
+    public void setUp() {
         System.out.println("Setting up driver");
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver1 = new ChromeDriver();
+        driver2 = new ChromeDriver();
     }
 
     @After
     public void tearDown() {
         System.out.println("Closing driver");
-        driver.quit();
+        driver1.quit();
+        driver2.quit();
+
     }
 }
