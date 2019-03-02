eb46e9e863d21067ac1a541dfac3a16ca7652483
PaulElcampeon <pauloladele@live.co.uk>
2019-03-02 18:42:35 +0000

    data transfer objects for forfeiting event

--- src/main/java/com/purgersmight/purgersmightapp/dto/ForfeitPlayerResDto.java
+++ src/main/java/com/purgersmight/purgersmightapp/dto/ForfeitPlayerResDto.java
@@ -1,4 +1,18 @@
 package com.purgersmight.purgersmightapp.dto;
 
+import com.purgersmight.purgersmightapp.models.PvpEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class ForfeitPlayerResDto {
+
+    private boolean ended;
+    private String winner;
+    private PvpEvent pvpEvent;
+
+    public ForfeitPlayerResDto(){}
+
+
 }
